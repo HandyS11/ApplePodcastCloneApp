8b0771a61e82a50117a771f338b9e840d2f661ac
etudiant <valentin-clergue@orange.fr>
2023-05-09 18:15:57 +0200

    Add empty workspace & app

---
+++ Sources/PodcastClone/PodcastClone/PodcastCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  PodcastCloneApp.swift
+//  PodcastClone
+//
+//  Created by etudiant on 09/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct PodcastCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
